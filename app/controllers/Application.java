6d7e9c7d5206eb2fceeb4539dd091f8a4a54cdc6
adeel718 <adeel.a.ahmed@accenture.com>
2014-04-28 14:45:03 +0100

    Created login page, not finished

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,23 @@
 package controllers;
 
+import static play.data.Form.form;
+import models.User;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(form(Login.class)));
     }
+    
+    public static class Login {
+		public String userName;
+
+		public String validate() {
+			return null;
+		}
+	}
 
 }
