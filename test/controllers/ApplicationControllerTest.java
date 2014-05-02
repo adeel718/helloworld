e9c4270973984dd74e2afdc7d4ade1f71ffa89d8
Mihails Cirganovics <mihails.cirganovics@accenture.com>
2014-05-02 09:59:43 +0100

    Controller unit test changed slightly

--- test/controllers/ApplicationControllerTest.java
+++ test/controllers/ApplicationControllerTest.java
@@ -1,39 +1,27 @@
 package controllers;
 
-import static org.junit.Assert.*;
-import static org.fest.assertions.Assertions.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import play.mvc.Http;
 import play.mvc.Result;
-import play.mvc.Http.Context;
-import play.mvc.Http.Flash;
 import play.test.FakeRequest;
 import play.test.Helpers;
 
 public class ApplicationControllerTest {
-	
-	@Before
-	public void setUp() {
-		final Context ctx = mock(Context.class);
-		final Flash mockFlash = new Flash(new HashMap<String, String>());
-		when(ctx.flash()).thenReturn(mockFlash);
-		Context.current.set(ctx);
-	}
 	
 	@Test
 	public void controllerShouldRenderLoginView() {
 		Result result =	Application.login();
 //		System.out.println(result.getWrappedResult().toString());
 		String s = Helpers.contentAsString(result);
-		assertTrue((s).contains("Please sign in"));
+		assertTrue((s).contains("Please sign in")); // should render login page
 		assertOk(result);
 	}
 	
@@ -47,7 +35,7 @@ public class ApplicationControllerTest {
 		Result result = Helpers.callAction(controllers.routes.ref.Application.loginSubmit(), fakeRequest.withFormUrlEncodedBody(data));
 		
 		String s = Helpers.contentAsString(result);
-		assertTrue((s).contains("Hello Adeel"));
+		assertTrue((s).contains("Hello Adeel")); // should render welcome page
 		assertOk(result);
 	}
 	
@@ -72,10 +60,18 @@ public class ApplicationControllerTest {
 	    final Result result = Helpers.callAction(controllers.routes.ref.Application.loginSubmit(), fakeRequest.withFormUrlEncodedBody(data));
 		
 		final String s = Helpers.contentAsString(result);
-		assertTrue((s).contains("Please sign in"));
-		assertTrue(s.contains("value=\"" + username + "\""));
-		assertTrue((s).contains("Please enter the user name field and it should not be blank"));
-		assertThat(Helpers.status(result)).isEqualTo(Http.Status.BAD_REQUEST);
+		assertTrue((s).contains("Please sign in")); // should render login page
+		assertTrue(s.contains("value=\"" + username + "\"")); // should prepopulate the username input field
+		assertContainsTwice(s, "Please enter the user name field and it should not be blank"); // should show error in summary and the input field
+		assertThat(Helpers.status(result)).isEqualTo(Http.Status.BAD_REQUEST); // should indicate an erroneous submission
+	}
+	
+	private void assertContainsTwice(String html, String errorMessage) {
+		assertNotNull(errorMessage);
+		final int firstIndexOf = html.indexOf(errorMessage);
+		assertThat(firstIndexOf).isPositive();
+		final int secondIndexOf = html.substring(firstIndexOf + errorMessage.length()).indexOf(errorMessage);
+		assertThat(secondIndexOf).isPositive();
 	}
 	
 	private void assertOk(Result result) {
