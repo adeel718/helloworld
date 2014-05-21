20093de17a9cf846c0da99c2e414c73efa598059
adeel718 <adeel.a.ahmed@accenture.com>
2014-05-21 11:39:30 +0100

    Registration controller & unit tests

--- app/controllers/UserRegistration.scala
+++ app/controllers/UserRegistration.scala
@@ -7,18 +7,19 @@ import play.api.data.Forms._
 import models.{User, Registration}
 import views.html.{register, login}
 import play.data.Form._
+import play.api.i18n.Messages
 
 /**
  * Created by user02 on 5/20/14.
  */
 object UserRegistration extends Controller {
 
-  val form =Form( mapping( "firstName" -> text.verifying("firstName should not be empty Please enter.", {!_.isEmpty}),
-    "surname" -> text.verifying("firstName should not be empty Please enter.", {!_.isEmpty}),
-    "email" -> email ,
-    "confirmEmail" -> email,
+  val form =Form( mapping( "firstName" -> text.verifying(Messages("error.firstName.required"), {!_.isEmpty}),
+    "surname" -> text.verifying("surname should not be empty Please enter.", {!_.isEmpty}),
+    "email" -> text.verifying("email should not be empty Please enter valid email.", {!_.isEmpty}) ,
+    "confirmEmail" -> text.verifying("Please confirm email.", {!_.isEmpty}) ,
      "password" -> text.verifying("password should not be empty Please enter.", {!_.isEmpty}) ,
-     "confirmPassword" -> text.verifying("Please confirm the password.", {!_.isEmpty}),
+     "confirmPassword" -> text.verifying("Please confirm entered password.", {!_.isEmpty}),
      "tconditions" -> checked("Please accept terms & conditions"))
       (Registration.apply)(Registration.unapply))
 
