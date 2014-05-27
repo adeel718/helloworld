1ee4c81a9b3dfe4d427a98e058edd2cdf31daea4
adeel718 <_AA51sunderland>
2014-05-27 09:30:12 +0100

    Selenium validation message test added

--- test/uk/gov/hmrc/RegistrationPageVerificationSeleniumTest.java
+++ test/uk/gov/hmrc/RegistrationPageVerificationSeleniumTest.java
@@ -12,6 +12,7 @@ import org.openqa.selenium.NoSuchElementException;
 
 import org.openqa.selenium.firefox.FirefoxDriver;
 import views.BaseSeleniumTest;
+import play.api.i18n.Messages;
 
 public class RegistrationPageVerificationSeleniumTest extends BaseSeleniumTest {
     @Before
@@ -25,7 +26,6 @@ public class RegistrationPageVerificationSeleniumTest extends BaseSeleniumTest {
 		driver.navigate().to(getBaseURL());
 		
 		//Main Verification Tests
-		
 		String verifyTextLinkExp = "GOV .UK";
 		String verifyTextLinkAct = driver.findElement(By.id("logo")).getText();
 		assertEquals(verifyTextLinkExp, verifyTextLinkAct);
@@ -54,121 +54,157 @@ public class RegistrationPageVerificationSeleniumTest extends BaseSeleniumTest {
 		
 	}
 		
-		@Test
-		public void registrationTests(){
-			
-		driver.navigate().to(getBaseURL() + "/register");
-		//Registration Page Tests
+    @Test
+    public void registrationTests(){
+
+        driver.navigate().to(getBaseURL() + "/register");
+        //Registration Page Tests
+
+        String verifyRegTitleExp = "Account Registration";
+        String verifyRegTitleAct = driver.findElement(By.id("title")).getText();
+        assertEquals(verifyRegTitleExp, verifyRegTitleAct);
+
+        String verifyRegDescriptionExp = "This page is the registration page for Hello World";
+        String verifyRegDescriptionAct = driver.findElement(By.id("pageDescription")).getText();
+        assertEquals(verifyRegDescriptionExp, verifyRegDescriptionAct);
+
+        String verifyRegPersonalDetailsTitleExp = "Your Personal Details";
+        String verifyRegPersonalDetailsTitleAct = driver.findElement(By.id("personalDetailsTitle")).getText();
+        assertEquals(verifyRegPersonalDetailsTitleExp, verifyRegPersonalDetailsTitleAct);
+
+        String verifyRegPersonalDetailsFNameExp = "First Name:";
+        String verifyRegPersonalDetailsFNameAct = driver.findElement(By.id("firstNameP")).getText();
+        assertEquals(verifyRegPersonalDetailsFNameExp, verifyRegPersonalDetailsFNameAct);
+
+        String verifyRegPersonalDetailsSNameExp = "Surname:";
+        String verifyRegPersonalDetailsSNameAct = driver.findElement(By.id("lastNameP")).getText();
+        assertEquals(verifyRegPersonalDetailsSNameExp, verifyRegPersonalDetailsSNameAct);
+
+        String verifyRegContactDetailsTitleExp = "Your Contact Details";
+        String verifyRegContactDetailsTitleAct = driver.findElement(By.id("contactDetailsTitle")).getText();
+        assertEquals(verifyRegContactDetailsTitleExp, verifyRegContactDetailsTitleAct);
+
+        String verifyRegContactDetailsCorrEmailExp = "Email:";
+        String verifyRegContactDetailsCorrEmailAct = driver.findElement(By.id("emailP")).getText();
+        assertEquals(verifyRegContactDetailsCorrEmailExp, verifyRegContactDetailsCorrEmailAct);
+
+        String verifyRegContactDetailsConfEmailExp = "Confirm Email:";
+        String verifyRegContactDetailsConfEmailAct = driver.findElement(By.id("confirmEmailP")).getText();
+        assertEquals(verifyRegContactDetailsConfEmailExp, verifyRegContactDetailsConfEmailAct);
+
+        String verifyRegPasswordTitleExp = "Your Password";
+        String verifyRegPasswordTitleAct = driver.findElement(By.id("passwordDetailsTitle")).getText();
+        assertEquals(verifyRegPasswordTitleExp, verifyRegPasswordTitleAct);
+
+        String verifyRegPasswordPwordExp = "Password:";
+        String verifyRegPasswordPwordAct = driver.findElement(By.id("passwordP")).getText();
+        assertEquals(verifyRegPasswordPwordExp, verifyRegPasswordPwordAct);
+
+        String verifyRegPasswordReExp = "Re-enter Password:";
+        String verifyRegPasswordReAct = driver.findElement(By.id("confirmPasswordP")).getText();
+        assertEquals(verifyRegPasswordReExp, verifyRegPasswordReAct);
+
+        String verifyRegTandCTitleExp = "Terms and Conditions";
+        String verifyRegTandCTitleAct = driver.findElement(By.id("termsTitle")).getText();
+        assertEquals(verifyRegTandCTitleExp, verifyRegTandCTitleAct);
+
+        String verifyRegTandCDescExp = "Before registering, please read our Terms and Conditions";
+        String verifyRegTandCDescAct = driver.findElement(By.id("TermsSubHeading")).getText();
+        assertEquals(verifyRegTandCDescExp, verifyRegTandCDescAct);
+
+        String verifyRegBoxTextExp = "I agree with the Terms and Conditions";
+        String verifyRegBoxTextAct = driver.findElement(By.id("TermsP")).getText();
+        assertEquals(verifyRegBoxTextExp, verifyRegBoxTextAct);
+
+        assertTrue(isElementPresent(By.name("tconditions")));
+    }
 		
-		String verifyRegTitleExp = "Account Registration";
-		String verifyRegTitleAct = driver.findElement(By.id("title")).getText();
-		assertEquals(verifyRegTitleExp, verifyRegTitleAct);
-		
-		String verifyRegDescriptionExp = "This page is the registration page for Hello World";
-		String verifyRegDescriptionAct = driver.findElement(By.id("pageDescription")).getText();
-		assertEquals(verifyRegDescriptionExp, verifyRegDescriptionAct);
-		
-		String verifyRegPersonalDetailsTitleExp = "Your Personal Details";
-		String verifyRegPersonalDetailsTitleAct = driver.findElement(By.id("personalDetailsTitle")).getText();
-		assertEquals(verifyRegPersonalDetailsTitleExp, verifyRegPersonalDetailsTitleAct);
-		
-		String verifyRegPersonalDetailsFNameExp = "First Name:";
-		String verifyRegPersonalDetailsFNameAct = driver.findElement(By.id("firstNameP")).getText();
-		assertEquals(verifyRegPersonalDetailsFNameExp, verifyRegPersonalDetailsFNameAct);
-		
-		String verifyRegPersonalDetailsSNameExp = "Surname:";
-		String verifyRegPersonalDetailsSNameAct = driver.findElement(By.id("lastNameP")).getText();
-		assertEquals(verifyRegPersonalDetailsSNameExp, verifyRegPersonalDetailsSNameAct);
-		
-		String verifyRegContactDetailsTitleExp = "Your Contact Details";
-		String verifyRegContactDetailsTitleAct = driver.findElement(By.id("contactDetailsTitle")).getText();
-		assertEquals(verifyRegContactDetailsTitleExp, verifyRegContactDetailsTitleAct);
-		
-		String verifyRegContactDetailsCorrEmailExp = "Correspondence Email:";
-		String verifyRegContactDetailsCorrEmailAct = driver.findElement(By.id("emailP")).getText();
-		assertEquals(verifyRegContactDetailsCorrEmailExp, verifyRegContactDetailsCorrEmailAct);
-		
-		String verifyRegContactDetailsConfEmailExp = "Confirm Email:";
-		String verifyRegContactDetailsConfEmailAct = driver.findElement(By.id("confirmEmailP")).getText();
-		assertEquals(verifyRegContactDetailsConfEmailExp, verifyRegContactDetailsConfEmailAct);
-		
-		String verifyRegPasswordTitleExp = "Your Password";
-		String verifyRegPasswordTitleAct = driver.findElement(By.id("passwordDetailsTitle")).getText();
-		assertEquals(verifyRegPasswordTitleExp, verifyRegPasswordTitleAct);
-		
-		String verifyRegPasswordPwordExp = "Password:";
-		String verifyRegPasswordPwordAct = driver.findElement(By.id("passwordP")).getText();
-		assertEquals(verifyRegPasswordPwordExp, verifyRegPasswordPwordAct);
-		
-		String verifyRegPasswordReExp = "Re-enter Password:";
-		String verifyRegPasswordReAct = driver.findElement(By.id("confirmPasswordP")).getText();
-		assertEquals(verifyRegPasswordReExp, verifyRegPasswordReAct);
-		
-		String verifyRegTandCTitleExp = "Terms and Conditions";
-		String verifyRegTandCTitleAct = driver.findElement(By.id("termsTitle")).getText();
-		assertEquals(verifyRegTandCTitleExp, verifyRegTandCTitleAct);
-		
-		String verifyRegTandCDescExp = "Before registering, please read our Terms and Conditions";
-		String verifyRegTandCDescAct = driver.findElement(By.id("TermsSubHeading")).getText();
-		assertEquals(verifyRegTandCDescExp, verifyRegTandCDescAct);
-		
-		String verifyRegBoxTextExp = "I agree with the Terms and Conditions";
-		String verifyRegBoxTextAct = driver.findElement(By.id("TermsP")).getText();
-		assertEquals(verifyRegBoxTextExp, verifyRegBoxTextAct);
-		
-		assertTrue(isElementPresent(By.name("tconditions")));
-		}
-		
-		@Test
-		public void linkTests(){
-			driver.navigate().to(getBaseURL() + "/register");
-		
-		//Links Navigation Tests
-				driver.findElement(By.cssSelector("img[alt=\"OGL\"]")).click();
-				assertEquals("Open Government Licence", driver.getTitle());
-				driver.navigate().back();
-				assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
-				
-				driver.findElement(By.xpath(".//*[@id='logo']/span")).click();
-				assertEquals("Welcome to GOV.UK", driver.getTitle());
-				driver.navigate().back();
-				assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
-				
+    @Test
+    public void linkTests(){
+        driver.navigate().to(getBaseURL() + "/register");
+
+        //Links Navigation Tests
+        driver.findElement(By.cssSelector("img[alt=\"OGL\"]")).click();
+        assertEquals("Open Government Licence", driver.getTitle());
+        driver.navigate().back();
+        assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
+
+        driver.findElement(By.xpath(".//*[@id='logo']/span")).click();
+        assertEquals("Welcome to GOV.UK", driver.getTitle());
+        driver.navigate().back();
+        assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
+
 //			    driver.findElement(By.linkText("Login")).click();	     
 //			    assertEquals("Please sign in", driver.getTitle());
-				
-			    driver.findElement(By.linkText("Open Government Licence v2.0")).click();
-		        assertEquals("Open Government Licence", driver.getTitle());
-				driver.navigate().back();
-			    assertEquals("Please sign in", driver.getTitle());
-		      
-			    driver.findElement(By.linkText("© Crown Copyright")).click();
-			    assertEquals("Crown copyright | The National Archives", driver.getTitle());
-				driver.navigate().back();
-			    assertEquals("Please sign in", driver.getTitle());
 
-			    driver.findElement(By.linkText("Find out more about cookies")).click();
-			    assertEquals("Cookies - GOV.UK", driver.getTitle());
-				driver.navigate().back();
-			    assertEquals("Please sign in", driver.getTitle());
-			      
-				driver.findElement(By.id("proposition-name")).click();
-				assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
+        driver.findElement(By.linkText("Open Government Licence v2.0")).click();
+        assertEquals("Open Government Licence", driver.getTitle());
+        driver.navigate().back();
+        assertEquals("Please sign in", driver.getTitle());
+
+        driver.findElement(By.linkText("© Crown Copyright")).click();
+        assertEquals("Crown copyright | The National Archives", driver.getTitle());
+        driver.navigate().back();
+        assertEquals("Please sign in", driver.getTitle());
+
+        driver.findElement(By.linkText("Find out more about cookies")).click();
+        assertEquals("Cookies - GOV.UK", driver.getTitle());
+        driver.navigate().back();
+        assertEquals("Please sign in", driver.getTitle());
+
+        driver.findElement(By.id("proposition-name")).click();
+        assertTrue(driver.getCurrentUrl().endsWith(getBaseURL()));
+
+    }
+
+    @Test
+    public void checkValidationMessages() {
+        driver.navigate().to((getBaseURL() + "/register"));
+
+        // Submit blank
+        driver.findElement(By.id("submitBtn")).click();
+
+        String validationMsgFirstNameExp = "firstName should not be empty, please enter a value";
+        String validationMsgFirstNameAct = driver.findElement(By.id("error1")).getText();
+        assertEquals(validationMsgFirstNameExp, validationMsgFirstNameAct);
+
+        String validationMsgSurnameExp = "surName should not be empty, please enter a value";
+        String validationMsgSurnameAct = driver.findElement(By.id("error2")).getText();
+        assertEquals(validationMsgSurnameExp, validationMsgSurnameAct);
+
+        String validationMsgEmailExp = "Email should not be empty, please enter a value";
+        String validationMsgEmailAct = driver.findElement(By.id("error3")).getText();
+        assertEquals(validationMsgEmailExp, validationMsgEmailAct);
+
+        String validationMsgConfirmEmailExp = "Please confirm email";
+        String validationMsgConfirmEmailAct = driver.findElement(By.id("error4")).getText();
+        assertEquals(validationMsgConfirmEmailExp, validationMsgConfirmEmailAct);
+
+        String validationMsgPasswordExp = "password should not be empty, please enter a value";
+        String validationMsgPasswordAct = driver.findElement(By.id("error5")).getText();
+        assertEquals(validationMsgPasswordExp, validationMsgPasswordAct);
+
+        String validationMsgConfirmPasswordExp = "Please confirm password";
+        String validationMsgConfirmPasswordAct = driver.findElement(By.id("error6")).getText();
+        assertEquals(validationMsgConfirmPasswordExp, validationMsgConfirmPasswordAct);
+
+        String validationMsgTermsExp = "Please accept terms & conditions";
+        String validationMsgTermsAct = driver.findElement(By.id("error7")).getText();
+        assertEquals(validationMsgTermsExp, validationMsgTermsAct);
+    }
 		
-	}
-		
-		@After
-		public void myAfter(){
-			driver.quit();
-			
-		}
+    @After
+    public void myAfter(){
+        driver.quit();
+
+    }
 
 	 private boolean isElementPresent(By by) {
-		    try {
-		      driver.findElement(by);
-		      return true;
-		    } catch (NoSuchElementException e) {
-		      return false;
-		    }
-		  }	
+        try {
+          driver.findElement(by);
+          return true;
+        } catch (NoSuchElementException e) {
+          return false;
+        }
+      }
 }
