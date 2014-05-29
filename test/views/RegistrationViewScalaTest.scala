a76734902b3c96994ea2a57a27ee44b9b37b1307
MC <mihails.cirganovics@accenture.com>
2014-05-29 14:14:22 +0100

    Refactored and fixed RegistrationViewScalaTest

--- test/views/RegistrationViewScalaTest.scala
+++ test/views/RegistrationViewScalaTest.scala
@@ -1,10 +1,9 @@
 package views
 
 import org.junit.Test
-
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
-import play.test.Helpers._
+import play.api.test.Helpers._
 import play.api.templates.Html
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
@@ -13,13 +12,16 @@ import play.api.data.Form
 import play.api.data.Forms._
 import models.Registration
 import play.api.i18n.Messages
+import play.api.test.FakeRequest
+import play.api.test.FakeApplication
 
 /**
  * Created by user02 on 5/20/14.
  */
 class RegistrationViewScalaTest extends FunSuite with Matchers {
 
-  val form =Form( mapping( "firstName" -> text.verifying(Messages("error.registration.field.required", "firstName"), {!_.isEmpty}),
+  
+  def form =Form( mapping( "firstName" -> text.verifying(Messages("error.registration.field.required", "firstName"), {!_.isEmpty}),
     "surname" -> text.verifying(Messages("error.registration.field.required", "surname"), {!_.isEmpty}),
     "email" -> text.verifying(Messages("error.email.required"), {!_.isEmpty}) ,
     "confirmEmail" -> text.verifying(Messages("error.confirmEmail.required"), {!_.isEmpty}) ,
@@ -29,8 +31,7 @@ class RegistrationViewScalaTest extends FunSuite with Matchers {
     (Registration.apply)(Registration.unapply))
 
   test("Check if labels and fields exist on registration screen") {
-    running(fakeApplication, new Runnable {
-      def run {
+    running(FakeApplication()) {
         //Context.current.set(TestSetup.testHttpContext)
         val html: Html = views.html.register(form)
         val doc: Document = Jsoup.parse(contentAsString(html))
@@ -67,29 +68,38 @@ class RegistrationViewScalaTest extends FunSuite with Matchers {
         doc.select("#agreeCbxLabel").text() should(include ("I agree with Terms & Conditions"))
 
         doc.select("#submitBtn").attr("value") should(equal ("Proceed>>"))
-      }
-    })
+    }
   }
 
   test("Check if validation is in place and error messages shown") {
-    //val data = Map("")
 
+    running(FakeApplication()) {
+    	// here we "submit" the empty fields
+        val fakeRequest = FakeRequest(POST, "/register").withFormUrlEncodedBody(
+       		"firstName" -> "",
+		    "surname" -> "",
+		    "email" -> "",
+		    "confirmEmail" -> "",
+		    "password" -> "",
+		    "confirmPassword" -> ""
+//		    "tconditions" -> ""
+	    )
 
-    running(fakeApplication, new Runnable {
-      def run {
-        //val fakeRequest = fakeRequest(POST, "/register")
-
-        val html: Html = views.html.register(form)
+	    // here we bind form from the empty fields in request and get the form with validation errors
+	    // rendering the form with validation errors should display the error messages by the fields
+        val html: Html = views.html.register(form.bindFromRequest()(fakeRequest))
         val doc: Document = Jsoup.parse(contentAsString(html))
 
-        /*doc.select("#validation-summary").text should(include (Messages("error.registration.field.required", "firstName")))
+        // expect the validation errors appear in a summary section 
+        doc.select("#validation-summary").text should(include (Messages("error.registration.field.required", "firstName")))
         doc.select("#validation-summary").text should(include (Messages("error.registration.field.required", "surname")))
-        doc.select("#validation-summary").text should(include (Messages("error.email.field.required", "email")))
-        doc.select("#validation-summary").text should(include (Messages("error.confirmEmail.field.required", "confirmEmail")))
+        doc.select("#validation-summary").text should(include (Messages("error.email.required")))
+        doc.select("#validation-summary").text should(include (Messages("error.confirmEmail.required")))
         doc.select("#validation-summary").text should(include (Messages("error.registration.field.required", "password")))
-        doc.select("#validation-summary").text should(include (Messages("error.confirmPassword.field.required", "confirmPassword")))
-        doc.select("#validation-summary").text should(include (Messages("error.tconditions.field.required", "tconditions")))*/
+        doc.select("#validation-summary").text should(include (Messages("error.confirmPassword.required")))
+        doc.select("#validation-summary").text should(include (Messages("error.tconditions.required")))
 
+        // expect the validation errors appear at the each field 
         doc.select("#errorfirstName").text should(include ("firstName should not be empty Please enter."))
 
         doc.select("#errorsurname").text should(include ("surname should not be empty Please enter."))
@@ -102,7 +112,6 @@ class RegistrationViewScalaTest extends FunSuite with Matchers {
 
         doc.select("#errortConditions").text should(include ("Please accept terms & conditions"))
       }
-    })
   }
 
 }
