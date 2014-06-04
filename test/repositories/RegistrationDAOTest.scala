e7c8d9e228f73027822c302308e42d62f5bd124d
Adeel Ahmed <adeel.a.ahmed@accenture.com>
2014-06-04 09:22:20 +0100

    RegistrationDAOTest

--- test/repositories/RegistrationDAOTest.scala
+++ test/repositories/RegistrationDAOTest.scala
@@ -7,7 +7,8 @@ import models._
 import util.RegistrationTestDBUtil
 import com.mongodb.casbah.commons.MongoDBObject
 import org.scalatest.OptionValues._
-
+import play.api.test.FakeApplication
+import play.api.test.Helpers.running
 
 /**
  * Created by user02 on 5/30/14.
@@ -18,19 +19,19 @@ class RegistrationDAOTest extends FunSuite with Matchers{
 
   test ("Create User Registration")
   {
+    running(FakeApplication){
      val userRegistration = models.RegisteredUser(surname = "Sachin",
       firstName ="Tendulkar",
       email="100@sachin.com",
       password="password")
-    RegisteredUserDAO.insert(userRegistration)
+      RegisteredUserDAO.insert(userRegistration)
 
-   val actualData = RegistrationTestDBUtil.findOne(MongoDBObject("_id" -> userRegistration.email, "password" -> userRegistration.password)).value
-
-    actualData.email shouldEqual userRegistration.email
-    actualData.surname shouldEqual userRegistration.surname
-    actualData.firstName shouldEqual userRegistration.firstName
-    actualData.password shouldEqual userRegistration.password
-
+	   val actualData = RegistrationTestDBUtil.findOne(MongoDBObject("_id" -> userRegistration.email, "password" -> userRegistration.password)).value
+	    actualData.email shouldEqual userRegistration.email
+	    actualData.surname shouldEqual userRegistration.surname
+	    actualData.firstName shouldEqual userRegistration.firstName
+	    actualData.password shouldEqual userRegistration.password
+    }
   }
 
 }
